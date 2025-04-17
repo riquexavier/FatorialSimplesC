b425e295ba4e17d2427d79d5f37c19509b52b1f4
riquexavier <pedrooohhenriquex@hotmail.com>
2025-04-17 20:17:16 -0300

    feito

--- main.c
+++ main.c
@@ -3,6 +3,17 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int n, i, f=1;
+
+    scanf("%i", &n);
+
+    for (n = n; n > 1; n--)
+    {
+        f = f * n;
+    }
+
+    printf("%i\n", f);
+
+    system("pause");
     return 0;
 }
